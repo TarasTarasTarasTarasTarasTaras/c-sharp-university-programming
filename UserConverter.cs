8975408730fabb6893c2bca643168d0c55f13676
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2022-04-04 19:14:17 +0300

    Add files via upload

---
+++ UserConverter.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Staff_Project
+{
+    using Models.User;
+
+
+    public class UserConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(User));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            if (jo["Role"].Value<string>() == "1")
+                return jo.ToObject<Staff>(serializer);
+
+            if (jo["Role"].Value<string>() == "0")
+                return jo.ToObject<Admin>(serializer);
+
+            return null;
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
