798651ee28ebd08a7a1a0912b449716221215959
Taras Vynar <90195508+TarasTarasTarasTarasTarasTaras@users.noreply.github.com>
2022-04-04 19:13:37 +0300

    Add files via upload

---
+++ Models/User/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Staff_Project.Models.User
+{
+    using System.Collections.Generic;
+
+    public abstract class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Role Role { get; set; }
+        public List<Payment> Payments { get; set; }
+
+        /*protected User(string firstName, string lastName, string email, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            Payments = new List<Payment>();
+        }*/
+    }
+}
